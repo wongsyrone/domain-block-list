1612f1c34f0535feae383035fc686f890add2738
Syrone Wong <wong.syrone@gmail.com>
2015-09-21 20:33:55 +0800

    split ipset list; misc cleanup

--- dnsmasq-conf-maker.sh
+++ dnsmasq-conf-maker.sh
@@ -2,43 +2,45 @@ ConfFile="./dnsmasq-blocklist.conf"
 DefaultDomainList="./domains.txt"
 DomainList="$1"
 
-if [ -f "$DefaultDomainList" ]; then
-	DomainListExists=1
-else
-	DomainListExists=0
-fi
+[ -f "$DefaultDomainList" ] && DomainListExists=1 || DomainListExists=0
 
-if [ "$1" == "" -a "$DomainListExists" == "0" ]; then
+[ "$1" == "" -a "$DomainListExists" == "0" ] && {
 	echo "Notice: must cleanup old files before!"
 	echo "the Default Domain List is $DefaultDomainList"
 	echo "usage: $0 DOMAIN_LIST_FILE"
 	exit 1
-fi
+}
 
-if [ "$1" == "" -a "$DomainListExists" == "1" ]; then
+[ "$1" == "" -a "$DomainListExists" == "1" ] && {
 	echo "using DefaultDomainList: $DefaultDomainList"
 	DomainList=${DefaultDomainList}
-fi
+}
 
 rm -f $ConfFile 2>&1
 
-echo -n "Enter your dns server (syntax IP[#port] ):"
-read dnsserver
-if [ "$dnsserver" == "" ]; then
-	dnsserver="8.8.8.8"
-	echo "dns server empty, using 8.8.8.8 instead"
-fi
-# in case using ':'
-dnsserver=${dnsserver//:/#}
+echo -n "Only need ipset list? [y/n]:"
+read NoServer
+[ "$NoServer" == "y" -o "$NoServer" == "Y" ] || {
+	echo -n "Enter your dns server (syntax IP[#port] ):"
+	read dnsserver
+	[ "$dnsserver" == "" ] && {
+		dnsserver="8.8.8.8"
+		echo "dns server empty, using 8.8.8.8 instead"
+	}
+	# in case using ':'
+	dnsserver=${dnsserver//:/#}
+}
 echo -n "Enter your ipset name:"
 read ipset
-if [ "$ipset" == "" ]; then
+[ "$ipset" == "" ] && {
 	ipset="UNKNOWN_IPSET_NAME"
 	echo "ipset name empty!!"
-fi
+}
 
 cat $DomainList | while read SingleDomain
 do
 	echo "ipset=/$SingleDomain/${ipset}">>$ConfFile
-	echo "server=/$SingleDomain/${dnsserver}">>$ConfFile
+	[ "$NoServer" == "y" -o "$NoServer" == "Y" ] || {
+		echo "server=/$SingleDomain/${dnsserver}">>$ConfFile
+	}
 done
